1d9be259bcabfec9112c3b7d68eef5bd0817537f
unknown <nathanani65@nu.ac.th>
2025-12-23 15:21:48 +0700

    Refactor to MVC: Added DrinkController and routes

---
+++ nv-webblog68-main/nv-webblog68/server/src/app.js
@@ -0,0 +1,16 @@
+let express = require('express')
+let bodyParser = require('body-parser')
+ 
+const app = express()
+ 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+// เรียกใช้ routes โดยส่ง app เข้าไป
+require('./routes')(app)
+
+let port = 8081
+app.listen(port, function () {
+    console.log('server running on ' + port)
+})
+ 
